e1005760c04585945f1ed6a903c05ec1140a9f46
isdelr <rosvelt005@hotmail.com>
2025-08-01 16:32:16 +0000

    feat(design): Incorporate colors and font from Figma.
    Made a tailwind config aligned to the figma design guidelines, added the font and updated tailwind to a newer minor version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Archivo } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Font defined in the Figma design
+const archivo = Archivo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Apply Digital Test",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={archivo.className}>{children}</body>
     </html>
   );
 }
