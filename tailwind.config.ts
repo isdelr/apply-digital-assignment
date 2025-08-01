9580d1e15c4a93d331c37d1ecbfa08f5fc37b8db
isdelr <rosvelt005@hotmail.com>
2025-08-01 16:32:16 +0000

    feat(design): Incorporate colors and font from Figma.
    Made a tailwind config aligned to the figma design guidelines, added the font and updated tailwind to a newer minor version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    //Properties based on the design in the figma page
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "bg-primary": "#585660",
+        "bg-neutral": "#404040",
+        "surface-secondary": "#EEEEEE",
+        "stroke-primary": "#3B3B3B",
+        "stroke-secondary": "#8F8F8F",
+        "stroke-tertiary": "#EFEDF3",
+        "text-primary": "#3B3B3B",
+        "text-secondary": "#737373",
       },
+      borderWidth: {
+        "card": "0.5px",
+        "cta": "1px",
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
