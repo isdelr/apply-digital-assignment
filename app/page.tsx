2299615ca847573ce4164950bf9d573a515c1aac
isdelr <rosvelt005@hotmail.com>
2025-08-01 19:07:15 -0400

    feat(component): Initial implementation of catalog header
    Made the catalog-header component alongside a select component to filter by genre in a future PR.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import CatalogHeader from "@/components/home/catalog-header";
+
 export default async function Home() {
   return (
     <div>
-     
+      <div className="md:px-32 px-6 md:pt-12 pt-8 md:pb-12 pb-8">
+        <CatalogHeader availableFilters={[]} title="Top Sellers" />
+      </div>
     </div>
-  )
+  );
 }
