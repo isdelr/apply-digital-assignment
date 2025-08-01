bf73c2d72d0ca16331ccd78dbc884ba03801d000
isdelr <rosvelt005@hotmail.com>
2025-08-01 20:25:52 +0000

    feat(component): Added icon component
    Made an icon component and added the svgr webpack dependency to be able to use svgs directly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //Added svgr to be able to use svgs as react components
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: /url/ },
+        use: ["@svgr/webpack"],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+};
 
 export default nextConfig;
