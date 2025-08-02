93754f3edaf9becbb0725bfb6c292ff7edf3eaf1
isdelr <rosvelt005@hotmail.com>
2025-08-01 22:29:31 -0400

    feat(functionality): Added pagination and filters to catalog grid
    Added pagination using the See more button and filtering using the select.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,11 @@
-export default function Loading() {
-  return <p>Loading...</p>
+import Icon from "@/components/ui/icon";
+import SpinnerIcon from "@/public/icons/spinner.svg";
+
+export default function Loading() {        
+  return (
+    <div className="flex flex-col justify-center h-dvh items-center gap-8">
+      <Icon icon={SpinnerIcon} className="size-32 text-gray-200 animate-spin fill-bg-primary" />
+      <span className="text-text-primary font-bold text-xl">Loading Games...</span>
+    </div>
+  );
 }
--- app/utils/endpoint.ts
+++
@@ -1,6 +0,0 @@
-export const availableFilters = Array.from(
-  new Set(allGames.map((game) => game.genre))
-);
-
-export const delay = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
