3d8a97e5c4cfba7bca6d02a793318f1bf7ee9b44
isdelr <rosvelt005@hotmail.com>
2025-08-01 20:45:30 -0400

    refactor(structure): Refactored project and services
    Added a service to use the games api route and made the games a json file in the public folder to make the test more realistic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import CatalogHeader from "@/components/home/catalog-header";
+import { getGames } from "./services/games";
 
 export default async function Home() {
+  const data = (await getGames());
+
   return (
     <div>
       <div className="md:px-32 px-6 md:pt-12 pt-8 md:pb-12 pb-8">
-        <CatalogHeader availableFilters={[]} title="Top Sellers" />
+        <CatalogHeader availableFilters={data.availableFilters} title="Top Sellers" />
       </div>
     </div>
   );
---
+++ app/services/games.ts
@@ -0,0 +1,41 @@
+import { Game } from "@/app/types"
+
+export interface GamesApiResponse {
+  games: Game[];
+  availableFilters: string[];
+  totalPages: number;
+  currentPage: number;
+}
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+/**
+ * Fetches a list of games from the API.
+ * @param genre - The genre to filter by.
+ * @param page - The page number for pagination.
+ * @returns A promise that resolves to the API response.
+ */
+export const getGames = async (
+  genre?: string | null,
+  page: number = 1
+): Promise<GamesApiResponse> => {
+  // Construct the URL with query parameters
+  const url = new URL(`${API_BASE_URL}/games`);
+  if (genre && genre.toLowerCase() !== "all") {
+    url.searchParams.append("genre", genre);
+  }
+  url.searchParams.append("page", String(page));
+
+  try {
+    const response = await fetch(url.toString());
+    if (!response.ok) {
+      throw new Error("Failed to fetch games");
+    }
+    const data: GamesApiResponse = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching games:", error);
+
+    throw error;
+  }
+};
