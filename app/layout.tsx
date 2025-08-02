c41226378b468e022e91e31502dfeb0bc5fd41ce
isdelr <rosvelt005@hotmail.com>
2025-08-01 22:04:14 -0400

    feat(component): Added footer component
    Added footer component and logo that redirects to /

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Archivo } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/navbar";
 import CartIcon from "@/public/icons/cart.svg";
+import Footer from "@/components/shared/footer";
 
 // Font defined in the Figma design
 const archivo = Archivo({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={archivo.className}>
         <Navbar brandText="GamerShop" homeRoute="/" items={[{ icon: CartIcon, text: "Cart", url: "/cart" }]} />
         {children}
+        <Footer/>
       </body>
     </html>
   );
